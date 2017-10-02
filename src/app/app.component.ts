af3bc25e5a7fbadcc5e41ad761bd08f1def4d37c
ryanzam <ryanzam2005@yahoo.com>
2017-10-02 12:32:04 +0300

    article component created for listing titles and their urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log(`Adding title: ${title.value} and Link: ${link.value}`);
+    return false;
+  }
 }
