671e9af9e3cc85a1db70c38c8bf0a31f389cd82a
ryanzam <ryanzam2005@yahoo.com>
2017-10-03 11:39:34 +0300

    adding article model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor()
+  {
+    this.articles = [
+      new Article('Angular 4', 'http://anuglar.io', 8),
+      new Article('YouTube', 'http://youtube.com', 11),
+      new Article('Ramzan', 'http://ranjamalee.com', 50)
+    ]
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
     console.log(`Adding title: ${title.value} and Link: ${link.value}`);
     return false;
