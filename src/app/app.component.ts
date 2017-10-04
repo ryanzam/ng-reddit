1e24c4d19f58c7d7e91009f0e5e7898e357cd70e
ryanzam <ryanzam2005@yahoo.com>
2017-10-04 08:39:16 +0300

    voting correction and sorting added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,14 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
     console.log(`Adding title: ${title.value} and Link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes-a.votes);
+  }
+
 }
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -14,6 +14,7 @@ export class Article {
 
   voteUp(): void {
     this.votes += 1;
+    console.log('voteup clicked');
   }
 
   voteDown(): void{
